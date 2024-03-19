17a871a15cffa4c36e7af2c3cfabec2c9b63e2db
chrisahart <chrissahart@gmail.com>
2024-03-19 15:27:31 +0000

    Add li-chain

---
+++ examples/li-chain/cp2k-smeagol/iv/kpoints-1-1-20/analyse_iv.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+param_1=$(seq -2 0.5 2)
+
+job_directory=iv_curve
+output_file=IV.log
+
+rm $output_file
+#echo "IV curve" > $output_file
+
+for ii in $param_1 ; do
+        work_dir=$job_directory/V_${ii}
+        echo ${ii}
+        
+	charge="$(grep 'Total Charge' iv_curve/V_${ii}/log_4_${ii}V.out | awk '{print $3}')"
+        scf="$(grep 'scf_env_do_scf_inner_loop' iv_curve/V_${ii}/log_4_${ii}V.out | awk '{print $2}')"
+        current="$(cat $work_dir/${ii}V.CUR | awk '{print $2}')"
+        
+	#echo "${ii} $current $charge $scf"  >> $output_file
+	echo "${ii} $current"  >> $output_file
+
+done
+
+sed -i -e "s/D/E/g"  $output_file
+
+wait
